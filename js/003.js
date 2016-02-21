26db7c70e0dee47bce4318e7daa7f3990634e069
Michael Yuen <michaelclaytonyuen@gmail.com>
2016-02-21 13:09:55 -0500

    Revised 003 solution and added performace logging

--- js/003.js
+++ js/003.js
@@ -2,28 +2,46 @@
 	The prime factors of 13195 are 5, 7, 13 and 29.
 
 	What is the largest prime factor of the number 600851475143 ?
+
+	-------------------------------------------------------------
+
+	Notes:
+	
+	I began with a solution intended to solve the direct question.
+	-----
+	A second iteration found only unique prime factors, but I decided this was not the best approach.
+	It involved an additional function, checking each i for primeness.
+	-----
+	Now, I have expanded my solution to generally find prime factors.
+	This does not require checking for factor primeness, because it first removes all multipes of the prime.
+	Thus, higher non-prime multiples of the prime are avoided.
+
+	Finally, the largest will simply be the last.
  */
 exports.solution003 = function(){
 
-	var prime;
+	var i = 2, prime_factors = [], num = 600851475143;
 
-	for( var i=(30 - 1); i > 1; i-- ){
-
-		if( isPrime( i ) ){
-			console.log(i);
-			prime = i;
-		}
+	if( num < i ){
+		console.log('No prime factors.')
 	}
-	//console.log(prime);
-}
-
-function isPrime( i ){
-
-	for( var x=(i - 1); x > 1; x--){
-		if( i % x === 0){
-			return false;
-		}
+	else if( num === i ){
+		console.log('Prime factor: ' + num);
 	}
+	else{
 
-	return true;
+		while( i < num ){
+
+			if( num % i === 0 ){
+				prime_factors.push(i);
+				num /= i;
+			}
+			else{
+				i++;
+			}
+		}
+		prime_factors.push(num);
+
+		console.log(prime_factors);
+	}
 }
--- js/app.js
+++ js/app.js
@@ -9,7 +9,10 @@ if( arg ){
 
 	// Dynamically create function name because it can't be a number
 	problem_number = 'solution' + problem_number;
+
+	console.time(problem_number);
 	problem[problem_number]();
+	console.timeEnd(problem_number);
 }
 else{
 	console.log('You need to pass an argument to the problem to run: -XXX');
