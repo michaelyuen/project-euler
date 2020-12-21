0f2ae9037174d67780bf6fea573cb5803e18cd93
Michael Yuen <iam@mikey.baby>
2020-12-21 15:48:05 -0500

    Add solution 9

--- js/009.js
+++ js/009.js
@@ -7,45 +7,30 @@
 	There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 	Find the product abc.
  */
-exports.solution009 = function(){
-	
-	var found = false, triplet, c = 5;
-	while( !found ){
-
-		triplet = checkForTriplet( c );
-
-		if( triplet ){
-			
-			console.log( triplet[0] + ', ' + triplet[1] + ', ' + triplet[2] + ': ' + (triplet[0] + triplet[1] + triplet[2] ) );
-
-			if( triplet[0] + triplet[1] + triplet[2] === 1000 ){
-				
-				found = true;
-				console.log( triplet[0] * triplet[1] * triplet[2] );
-			}
-		}
-
-		c++;
-	}
+const square = (x) => Math.pow(x, 2);
+const squareRoot = (x) => Math.sqrt(x);
+const areSpecial = ({ a, b, c }) => a + b + c === 1000;
+const areValid = ({ a, b, c }) => a < b && b < c;
+const findSpecialTriplet = () => {
+  for (let a = 0; a < 1000; a++) {
+    for (let b = 1; b < 1000; b++) {
+      const c = squareRoot(square(a) + square(b));
+      if (
+        isWhole(c) &&
+        isTriplet({ a, b, c }) &&
+        areSpecial({ a, b, c }) &&
+        areValid({ a, b, c })
+      ) {
+        return { a, b, c };
+      }
+    }
+  }
 };
+const isTriplet = ({ a, b, c }) => square(a) + square(b) === square(c);
+const isWhole = (x) => x % 1 === 0;
 
-function checkForTriplet( c ){
-
-	var c2 = Math.pow(c, 2), b2, a2;
-
-	for( var b=c-1; b > 0; b-- ){
-
-		for( var a=b-1; a > 0; a-- ){
-
-			b2 = Math.pow( b, 2 );
-			a2 = Math.pow( a, 2 );
-
-			if( a2 + b2 === c2 ){
-
-				return [ a, b, c ];
-			}
-		}
-	}
-
-	return false;
+exports.solution009 = () => {
+  const { a, b, c } = findSpecialTriplet();
+  console.log(`[SPECIAL TRIPLET] a: ${a}, b: ${b}, c: ${c}`);
+  console.log(`[PRODUCT] ${a * b * c}`);
 };
